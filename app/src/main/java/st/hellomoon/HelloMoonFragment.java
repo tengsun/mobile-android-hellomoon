f4f7e8c9fb2671c5877c676b991ad07d2e8fd5e6
Teng Sun <tengsun@cn.ibm.com>
2016-02-19 15:12:34 +0800

    keep fragment's data and state

--- app/src/main/java/st/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/st/hellomoon/HelloMoonFragment.java
@@ -22,6 +22,11 @@ public class HelloMoonFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
